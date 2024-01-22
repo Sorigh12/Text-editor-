d0909bcd4006caeb34542069b71611ef69150121
Sorigh <84138511+Sorigh12@users.noreply.github.com>
2024-01-22 05:30:01 -0500

    Update App.js

--- Text editor/App.js
+++ Text editor/App.js
@@ -42,6 +42,11 @@ export default function App() {
         })
     }
     
+    function deleteNote(event, noteId) {
+        event.stopPropagation()
+        setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
+    }
+    
     function findCurrentNote() {
         return notes.find(note => {
             return note.id === currentNoteId
@@ -63,6 +68,7 @@ export default function App() {
                     currentNote={findCurrentNote()}
                     setCurrentNoteId={setCurrentNoteId}
                     newNote={createNewNote}
+                    deleteNote={deleteNote}
                 />
                 {
                     currentNoteId && 
